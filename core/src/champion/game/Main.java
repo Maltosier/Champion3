50b8dae91af516a4758a8ea8e3a31dd4c6464fcf
Mursheed <king.of.slackers@gmail.com>
2017-02-27 21:25:55 -0500

    Did a basic sprite draw and walk.

--- core/src/champion/game/Main.java
+++ core/src/champion/game/Main.java
@@ -1,33 +1,57 @@
-package champion.game;
+package champion.Game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	Animation<TextureRegion> walkAnimation;
+	Texture walkSheet;
+	SpriteBatch spriteBatch;
+	
+	float stateTime;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		walkSheet = new Texture(Gdx.files.internal("sprites/champions/male1Walk.png"));
+		
+		TextureRegion [][] tmp = TextureRegion.split(walkSheet, 200, 200);
+		
+		TextureRegion [] walkFrames = new TextureRegion[7];
+		int index = 0;
+		for (int i = 0; i < 1; i++){
+			for(int j = 0; j < 7; j++){
+				walkFrames[index++] = tmp[i][j];
+			}
+		}
+		
+		walkAnimation = new Animation<TextureRegion>(0.1f, walkFrames);
+		
+		spriteBatch = new SpriteBatch();
+		stateTime = 0f;
+		
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stateTime += Gdx.graphics.getDeltaTime();
+		
+		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, 50, 50);
+		spriteBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		spriteBatch.dispose();
+		walkSheet.dispose();
 	}
 }
--- desktop/src/champion/game/desktop/DesktopLauncher.java
+++ desktop/src/champion/game/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import champion.Game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new Main(), config);
 	}
 }
